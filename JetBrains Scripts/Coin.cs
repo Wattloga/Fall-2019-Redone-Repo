a7d7b6072a4d6a5036b314701f34f946a277b452
Logan W <31257855+Wattloga@users.noreply.github.com>
2019-11-18 14:39:40 -0700

    Scripts in Unity

---
+++ JetBrains Scripts/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events
+[CreateAssetMenu]
+public class Coin : MonoBehaviour
+{
+    public UnityAction<int> coinAction;
+
+    public int value = 10;
+
+    public void Spend()
+    {
+        coinAction?.Invoke(value);
+    }
+}
