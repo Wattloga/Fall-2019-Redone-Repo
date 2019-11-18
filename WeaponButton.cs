d42ce86dfc19b8a8b78ecf42b54e119cd781fdc0
Logan W <31257855+Wattloga@users.noreply.github.com>
2019-11-18 14:50:22 -0700

    Add files via upload
    Scripts done in class on 11/18/2019. WeaponHandler, WeaponConfig, WeaponButton.

---
+++ WeaponButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+
+public class WeaponButton : MonoBehaviour
+{ 
+    public WeaponConfig weaponObj;
+    private Button weaponButton;
+    private Image buttonImg;
+    private Text buttonText;
+    private void Start()
+    {
+        weaponButton = GetComponent<Button>();
+        weaponButton.onClick.AddListener(weaponObj.RaiseFireAction);
+        buttonImg = GetComponent<Image>();
+        buttonImg.color = weaponObj.weaponColor;
+        buttonText = GetComponentInChildren<Text>();
+        buttonText.text = weaponObj.name + "Fire";
+        weaponObj.collectWeaponAction = OnCollectWeapon;
+        weaponButton.interactable = false;
+     }
+
+    public void OnCollectEnter()
+    {
+
+    }
+}
+
---
+++ WeaponConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+
+public class WeaponConfig : ScriptableObject
+{
+    public UnityAction weaponFireAction;
+    public Color weaponColor = Color.red;
+    public GameObject ammoObj;
+    public Color[] colorArray;
+    public float firePower = 0.1f;
+    public floatData playerHealth;
+
+    public void RaiseFireAction()
+    {
+        weaponFireAction?.Invoke();
+    }
+
+    public Color RandomColor()
+    {
+        return weaponColor;
+    }
+
+    public void DoDamge()
+    {
+        playerHealth.UpdateValue(firePower);
+    }
+}
